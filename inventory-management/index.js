3219ba70adcba57c743d9131c550ddcb8d87ea28
rahuldey2903 <rahuldey2903@gmail.com>
2026-02-21 13:50:36 +0530

    Practice day 1

--- inventory-management/index.js
+++ inventory-management/index.js
@@ -1,3 +1,19 @@
 
 
-console.log("Hello World!")
+// console.log("Hello World!")
+
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.get("/", (req, res)=>{
+    res.send("Hello Dey!")
+})
+
+app.get("/about", (req, res)=>{
+    res.send("My First Project")
+})
+
+app.listen(port, () => {
+console.log(`App is listening on port ${port}`)
+})
